2be7e3454ed7b8a4fae40095526e47c94ae92f14
anoriyuriy <anoriyuriy@gmail.com>
2025-10-22 21:33:02 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include 'includes/header.php';
+?>
+
+<section class="products" aria-label="Продукты Gulnaz Sweets">
+  <div class="product">
+    <img src="ptimilk.jpg" alt="Конфеты Птимилк" />
+    <h2>Конфеты Птимилк</h2>
+  </div>
+  <div class="product">
+    <img src="iriska.jpg" alt="Ириски сливочные" />
+    <h2>Ириски сливочные</h2>
+  </div>
+  <div class="product">
+    <img src="nuts.jpg" alt="Орешки со сгущенным молоком" />
+    <h2>Орешки со сгущенным молоком</h2>
+  </div>
+  <div class="product">
+    <img src="halva.jpg" alt="Халва натуральная" />
+    <h2>Халва натуральная</h2>
+  </div>
+</section>
+
+<?php
+include 'includes/footer.php';
+?>

ee36b0fd473b7243888015eb78638a9ab7faa6ac
anoriyuriy <anoriyuriy@gmail.com>
2025-10-22 21:28:20 +0300

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Gulnaz Sweets</title>
+  <link href="https://fonts.googleapis.com/css2?family=Rouge+Script&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="/includes/style.css" />
+</head>
+<body>
+<header>
+  <h1><a href="/" class="logo">Gulnaz Sweets</a></h1>
+</header>
+<main>

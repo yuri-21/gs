6d161e5a7a856e743a9c673c7e25d22dd2465c33
Yuri <anoriyuriy@gmail.com>
2025-10-25 23:33:37 +0300

    Обновление

--- order-ptimilk.php
+++ order-ptimilk.php
@@ -9,67 +9,50 @@
 <!-- order-ptimilk.php -->
 <link rel="stylesheet" href="order-ptimilk.css">
 
-<div class="order-block order-ptimilk">
-    <h3>Оформить заказ "Птимилк"</h3>
+<div class="order-ptimilk">
+  <div class="order-row" data-box-index="0">
 
-    <div id="ptimilk-order-list">
-        <!-- Первый блок коробки напрямую в HTML для простоты -->
-        <div class="ptimilk-box" data-box-index="0">
-            <label>
-                Выберите коробку:
-                <select class="box-size-select">
-                    <option value="3">3 конфеты</option>
-                    <option value="9" selected>9 конфет</option>
-                    <option value="16">16 конфет</option>
-                </select>
-            </label>
+    <label class="box-size-label">
+      Коробка:
+      <select class="box-size-select">
+        <option value="3">3 шт</option>
+        <option value="9" selected>9 шт</option>
+        <option value="16">16 шт</option>
+      </select>
+    </label>
 
-            <div class="flavors-row">
-                <div class="flavor-item" data-flavor-id="cocoa">
-                    <img src="cocoa.jpg" alt="Cocoa" title="Cocoa" />
-                    <label>
-                        <button type="button" class="flavor-minus">×</button>
-                        <input type="number" min="0" max="9" value="3" class="flavor-qty-input" />
-                    </label>
-                </div>
-                <div class="flavor-item" data-flavor-id="strawberry">
-                    <img src="strawberry.jpg" alt="Strawberry" title="Strawberry" />
-                    <label>
-                        <button type="button" class="flavor-minus">×</button>
-                        <input type="number" min="0" max="9" value="2" class="flavor-qty-input" />
-                    </label>
-                </div>
-                <div class="flavor-item" data-flavor-id="coconut">
-                    <img src="coconut.jpg" alt="Coconut" title="Coconut" />
-                    <label>
-                        <button type="button" class="flavor-minus">×</button>
-                        <input type="number" min="0" max="9" value="2" class="flavor-qty-input" />
-                    </label>
-                </div>
-                <div class="flavor-item" data-flavor-id="pistachio">
-                    <img src="pistachio.jpg" alt="Pistachio" title="Pistachio" />
-                    <label>
-                        <button type="button" class="flavor-minus">×</button>
-                        <input type="number" min="0" max="9" value="2" class="flavor-qty-input" />
-                    </label>
-                </div>
-            </div>
-
-            <label>
-                Количество коробок:
-                <input type="number" class="box-count-input" min="1" value="1" />
-            </label>
-
-            <div class="box-price">Цена: <span class="price-value">790</span> ₽</div>
-            <button type="button" class="remove-box-btn" style="display:none;">Удалить коробку</button>
-        </div>
+    <div class="flavors-inline">
+      <div class="flavor-inline" data-flavor-id="cocoa">
+        <img src="cocoa.jpg" alt="Какао" title="Какао" />
+        <button type="button" class="flavor-minus">×</button>
+        <input type="number" min="0" max="16" value="3" class="flavor-qty-input" />
+      </div>
+      <div class="flavor-inline" data-flavor-id="strawberry">
+        <img src="strawberry.jpg" alt="Клубника" title="Клубника" />
+        <button type="button" class="flavor-minus">×</button>
+        <input type="number" min="0" max="16" value="2" class="flavor-qty-input" />
+      </div>
+      <div class="flavor-inline" data-flavor-id="coconut">
+        <img src="coconut.jpg" alt="Кокос" title="Кокос" />
+        <button type="button" class="flavor-minus">×</button>
+        <input type="number" min="0" max="16" value="2" class="flavor-qty-input" />
+      </div>
+      <div class="flavor-inline" data-flavor-id="pistachio">
+        <img src="pistachio.jpg" alt="Фисташка" title="Фисташка" />
+        <button type="button" class="flavor-minus">×</button>
+        <input type="number" min="0" max="16" value="2" class="flavor-qty-input" />
+      </div>
     </div>
 
-    <button id="add-box-btn" type="button">Добавить коробку</button>
+    <label class="box-count-label">
+      Кол-во:
+      <input type="number" class="box-count-input" min="1" value="1">
+    </label>
 
-    <div id="total-price-container">Общая сумма: <span id="total-price">790</span> ₽</div>
+    <div class="box-price">Цена: <span class="price-value">790</span> ₽</div>
 
-    <button id="submit-order" type="button" disabled>Добавить в корзину</button>
+    <button type="button" class="remove-box-btn" style="display:none;">Удалить</button>
+  </div>
 </div>
 
 <script src="order-ptimilk.js"></script>

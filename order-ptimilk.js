4a61391e1c69be66bc7fb57c69ba4a49014c3f3c
Yuri <anoriyuriy@gmail.com>
2025-10-26 22:49:01 +0300

    Обновление

--- order-ptimilk.js
+++ order-ptimilk.js
@@ -70,7 +70,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Итоговый ряд
     const summary = document.createElement('div');
     summary.className = 'order-summary';
-    summary.innerHTML = `<span class="order-action" data-add>добавить набор</span></br>Всего коробок ${totalBoxes} на сумму ${formatSum(totalSum)}`;
+    summary.innerHTML = `<span class="order-action" data-add>добавить набор</span></br></br></br>Всего коробок ${totalBoxes} на сумму ${formatSum(totalSum)}`;
 
     root.append(summary);
 

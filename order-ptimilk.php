109f67abd871f77c77d87ae1ac2d06a83117f312
Yuri <anoriyuriy@gmail.com>
2025-11-01 00:28:36 +0300

    Обновление

--- order-ptimilk.php
+++ order-ptimilk.php
@@ -2,7 +2,6 @@
 
 <div id="order-ptimilk-root"></div>
 
-<!-- подключаем корзину -->
-<?php include 'cart.php'; ?>
+<!-- подключаем корзину <?php include 'cart.php'; ?> -->
 
 <script src="order-ptimilk.js"></script>

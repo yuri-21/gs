10c2b2768a91a0c3994b6c5ef0ae7390eea27af8
Yuri <anoriyuriy@gmail.com>
2025-10-31 21:58:41 +0300

    Обновление

--- cart.php
+++ cart.php
@@ -3,20 +3,5 @@ session_start();
 include 'header.php';
 ?>
 
-<main>
-  <div class="cart-page">
-    <div class="cart-content">
-      <h2>Корзина</h2>
-
-      <div id="cart-root">
-        <!-- Тут JS отрисует корзину: товары, итоги, кнопки -->
-      </div>
-
-      <a href="/" class="back-link">← Продолжить покупки</a>
-    </div>
-  </div>
-</main>
-
-<script src="/cart.js"></script>
 
 <?php include 'footer.php'; ?>

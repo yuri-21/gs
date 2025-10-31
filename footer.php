85ce61978d70cd59dd203a0d903fff39f1140c76
Yuri <anoriyuriy@gmail.com>
2025-10-31 22:08:58 +0300

    Обновление

--- footer.php
+++ footer.php
@@ -8,17 +8,5 @@
         </div>
         <p class="copyright">&copy; 2025 Все права защищены</p>
     </footer>
-<script>
-function orderProduct(productName) {
-    alert('Заказ продукта: ' + productName + '\nФункционал в разработке');
-    // Здесь будет логика добавления в корзину
-}
-
-function checkout() {
-    alert('Оформление заказа в разработке');
-    // Здесь будет логика оформления заказа
-}
-
-</script>
 </body>
 </html>
